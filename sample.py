85df9a9a6c37f87fa6df0a67250496cda0ae2aca
Prabhu <prabhu@pdx.edu>
2022-06-28 12:01:36 -0700

    added code to read JSON file

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 import datetime as dt
 
 start = dt.datetime.now()
@@ -9,6 +10,10 @@ print("###########")
 print(time.sleep(5))
 print(os.getcwd())
 print("###########")
+
+fo = open('data.json', 'r')
+data = json.load(fo)
+print(data[0])
 
 print(dt.datetime.now() - start)
 print("ENDING SCRIPT")
